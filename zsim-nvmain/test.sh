c9de80b9b4c2572f3ae09e566cb1dd6f821ce079
JasonLeeLJQ <lijiaqi_jason@163.com>
2019-05-28 21:08:20 +0800

    test

--- zsim-nvmain/test.sh
+++ zsim-nvmain/test.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 rm mem*
+
+./run.sh
+if [ $? -eq 0 ]; then
+    echo -e "[H] 编译done!\n"
+else
+    echo "[H] 编译failed!"
+    exit
+fi
+echo "----------------------------------------------------------------------"
 ./bin/zsim ./config/flat.cfg
+if [ $? -eq 0 ]; then
+    echo "----------------------------------------------------------------------"
+    echo "[H] 执行done!"
+else
+    echo "[H] 执行failed!"
+    exit
+fi
